ed31fe4c2a8c44bc20a7182f560072b2b1a30f56
Coursera <aide.abdel@gmail.com>
2023-04-16 21:05:04 -0400

    Bmi

--- frontend/src/components/calculators/Bmi.js
+++ frontend/src/components/calculators/Bmi.js
@@ -1,15 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import Appbar from "../appbar";
 import StaticBanner from "../banner/StaticBanner";
 import LayoutCalculator from "./LayoutCalculator";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import Footer from "../footer";
 
 function Bmi() {
-  const title = "CALCULATE YOUR ONE-REP MAX (1RM)";
+  let height = 0,
+    weight = 1;
+  const [bmiIndex, setbmiIndex] = useState(null);
+  const form = (
+    <Box
+      sx={{
+        height: 300,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <TextField
+        id="weight"
+        label="Your weight... (kgs)"
+        fullWidth
+        onChange={(e) => {
+          weight = e.target.value;
+        }}
+      />
+      <TextField
+        id="height"
+        label="Your height... (cm)"
+        fullWidth
+        onChange={(e) => {
+          height = e.target.value;
+        }}
+      />
+      <Button
+        variant="contained"
+        onClick={() => {
+          setbmiIndex((weight / ((height / 100) * (height / 100))).toFixed(2));
+        }}
+      >
+        CALCULATE{" "}
+      </Button>
+    </Box>
+  );
+  const result = (
+    <Box
+      sx={{
+        height: 300,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <Typography variant="h5">YOUR BODY MASS INDEX IS</Typography>
+      <Typography variant="h1" textAlign="center">
+        {bmiIndex}
+      </Typography>
+      <Typography variant="body1">
+        These calculations are based on averages.
+      </Typography>
+    </Box>
+  );
+
+  const title = "BMI CALCULATOR";
   const description =
-    "Use the calculator below for any lift to estimate your one-rep max based on the amount of weight you can lift on a given move, and the number of clean reps you can achieve before muscle failure.";
-  const titleExplanation = "WHY DO YOU NEED TO CALCULATE YOUR 1RM?";
-  const bodyExplanation =
-    "Strength coaches set up programs with percentages based on your 1RM because they don't know your actual strength level, but they know what percentages they want you to be using relative to your single-rep max. The calculator gives you all the relevant loads, which are done simply by multiplying the percentage by your 1RM.";
+    "Knowing BMI can help adult men and women understand their overall health. Use the BMI calculator below to determine your body mass index by inputting your height and weight.";
+  const titleExplanation = "UNDERSTANDING YOUR BODY MASS INDEX";
+  const bodyExplanation = (
+    <Box width="60%">
+      <Typography variant="body1">
+        Now that you know your body mass index you are one step closer to
+        mastering your overall health. Find out how to understand your BMI
+      </Typography>
+      <Typography variant="body1">
+        <b>If your BMI is below 18.5:</b> Your BMI is considered underweight.
+        Keep in mind that an underweight BMI calculation may pose certain health
+        risks. Please consult your healthcare provider for more information
+        about BMI calculations.
+      </Typography>
+      <Typography variant="body1">
+        <b>If your BMI is between 18.5 – 24.9:</b> Your BMI is considered
+        normal. This healthy weight helps reduce your risk of serious health
+        conditions and means you’re close to your fitness goals.
+      </Typography>
+      <Typography variant="body1">
+        <b>If your BMI is between 25 – 29.9: </b> You’re in the overweight
+        range. You are at increased risk for a variety of illnesses at your
+        present weight. You should lose weight by changing your diet and
+        exercising more.
+      </Typography>
+      <Typography variant="body1">
+        <b>If your BMI is above 30:</b> Your BMI is considered overweight. Being
+        overweight may increase your risk of cardiovascular disease. Consider
+        making lifestyle changes through healthy eating and fitness to improve
+        your health.
+      </Typography>
+      <Typography variant="body1">
+        Individuals who fall into the BMI range of 25 to 34.9, and have a waist
+        size of over 40 inches for men and 35 inches for women, are considered
+        to be at especially high risk for health problems.
+      </Typography>
+    </Box>
+  );
   return (
     <React.Fragment>
       <Appbar />
@@ -19,9 +112,10 @@ function Bmi() {
         description={description}
         titleExplanation={titleExplanation}
         bodyExplanation={bodyExplanation}
-        form={1}
-        result={2}
+        form={form}
+        result={result}
       />
+      <Footer />
     </React.Fragment>
   );
 }
--- frontend/src/components/calculators/LayoutCalculator.js
+++ frontend/src/components/calculators/LayoutCalculator.js
@@ -10,10 +10,10 @@ function LayoutCalculator({
   result,
 }) {
   return (
-    <Container>
-      <Typography variant="h4">{title}</Typography>
-      <Typography variant="body">{description}</Typography>
-      <Grid container>
+    <Container sx={{ marginTop: "1%" }}>
+      <Typography variant="h5">{title}</Typography>
+      <Typography variant="subtitle1">{description}</Typography>
+      <Grid container spacing={10}>
         <Grid item xs={8}>
           {form}
         </Grid>
@@ -21,8 +21,8 @@ function LayoutCalculator({
           {result}
         </Grid>
       </Grid>
-      <Typography variant="h4">{titleExplanation}</Typography>
-      <Typography variant="body">{bodyExplanation}</Typography>
+      <Typography variant="h5">{titleExplanation}</Typography>
+      <Typography variant="subtitle1">{bodyExplanation}</Typography>
     </Container>
   );
 }
