625881beaa496667d97e8e3cba9e81eed4629553
Coursera <aide.abdel@gmail.com>
2023-04-17 12:27:24 -0400

    adding modules for backend

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Grid, Link, Icon } from "@mui/material";
+import { AppBar, Toolbar, Typography, Grid, Link } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
