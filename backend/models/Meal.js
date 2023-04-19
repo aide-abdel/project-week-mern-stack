05a30a48e81e47dadcfa91b48628ec177054a69e
Coursera <aide.abdel@gmail.com>
2023-04-19 11:59:41 -0400

    Meal model

--- backend/models/Meal.js
+++ backend/models/Meal.js
@@ -3,7 +3,6 @@ const { default: mongoose } = require("mongoose");
 const mealSchema = new mongoose.Schema({
   meal_id: { type: String, required: true },
   user: {
-    type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
